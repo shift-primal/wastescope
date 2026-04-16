4e74be0a02f1b5986c15734319d3a33d7d125ecf
Kasper Haugestøl <kasper@haugestol.com>
2026-04-16 13:01:35 +0200

    pool drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,10 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-
+import { Pool } from 'pg';
 import * as schema from './schema.ts';
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL!,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+});
+
+export const db = drizzle(pool, { schema });
